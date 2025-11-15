7af27d1909a9c93e4a2eabf24b27931dd31340e2
mahaveer800 <mahaveer56it@gmail.com>
2025-11-15 23:40:19 +0530

    frammotion

--- src/Component/Skills.jsx
+++ src/Component/Skills.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import {
   FaHtml5,
   FaCss3Alt,
@@ -30,11 +31,30 @@ const Skills = () => {
   return (
     <>
     <section className=" flex flex-col items-center text-white py-12 px-6 md:px-20 ">
+        <motion.div
+        initial={{ opacity: 0, x: -200 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1.2 }}
+        viewport={{ once: false, amount: 0.2 }}
+        className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden"
+        id="Skills"
+      >
       <h2 className="text-3xl font-bold mb-4 text-purple-900">What I Do</h2>
+
+      </motion.div>
       <p className="text-md mb-8 max-w-2xl ">
         <span className='text-purple-900 font-bold text-2xl'>I’m </span>Mahaveer from Budaun, India. I’ve completed my BCA and currently pursuing Full Stack Web Development at PW Skills. I specialize in building responsive React apps, integrating APIs, and polishing UI/UX with animations and modern design.
       </p>
+
+
       <div className='h-4'></div>
+        <motion.div
+        initial={{ opacity: 0, x: 200 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1.2 }}
+        viewport={{ once: false, amount: 0.2 }}
+        
+      >
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
         {skills.map((skill, index) => (
           <div
@@ -46,7 +66,9 @@ const Skills = () => {
             <span className="text-sm font-medium">{skill.name}</span>
           </div>
         ))}
-      </div>
+        </div>
+        </motion.div>
+      {/* </div> */}
 
     </section>
     
